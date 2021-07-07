116b978d677aa97066502b1ecc0c9fb8112520a9
Rodrigo Santos <souza.rodrigo61@gmail.com>
2021-07-07 00:26:21 -0300

    :package: feat(architecture): base redux

---
+++ Pay Flow/System/Pay_FlowApp.swift
@@ -0,0 +1,20 @@
+//
+//  Pay_FlowApp.swift
+//  Pay Flow
+//
+//  Created by Rodrigo Santos on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Pay_FlowApp: App {
+    
+    private let manager = DependencyManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
